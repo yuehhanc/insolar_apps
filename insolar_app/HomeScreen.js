c3afbdce6c07926cfb945fd5b195ad715d14cd68
Yueh-Han <yuehhanc@andrew.cmu.edu>
2019-04-09 10:18:17 -0400

    add Scroll View

--- insolar_app/HomeScreen.js
+++ insolar_app/HomeScreen.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Button, FlatList, TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Button, FlatList, TouchableOpacity, ScrollView} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +20,11 @@ type Props = {};
 export default class App extends Component<Props> {
   state = {
     navibar_items: [
-      {name: 'AboutUs'},
-      {name: 'Press'},
-      {name: 'Partners'},
-      {name: 'Resources'},
-      {name: 'Login'},
+      {name: 'AboutUs', text: 'About Us'},
+      {name: 'Press', text: 'Press'},
+      {name: 'Partners', text: 'Partners'},
+      {name: 'Resources', text: 'Resources'},
+      {name: 'Login', text: 'Login'},
     ],
   }
 
@@ -37,19 +37,25 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Image style={styles.INSOLAR_logo} source={require('./static/img/INSOLAR-logo.png')} />
-        <View style={styles.bg} >
-          <Image style={styles.bg_img} source={require('./static/img/solar_board.jpeg')} />
-          <Text style={styles.slogan_1} >Going Solar</Text>
-          <Text style={styles.slogan_2} >Made Easy</Text>
-          <Text style={styles.slogan_3} >Same Installers, Same Panels. Better Prices</Text>
-          <View style={styles.btn_container}>
-            <Button
-              //onPress={this.buttonClickListener}
-              title="Get Free Proposal"
-              color="#FFFFFF"
-            />
+        <ScrollView style={styles.scrollable_container}>
+          <View style={styles.bg} >
+            <Image style={styles.bg_img} source={require('./static/img/solar_board.jpeg')} />
+            <Text style={styles.slogan_1} >Going Solar</Text>
+            <Text style={styles.slogan_2} >Made Easy</Text>
+            <Text style={styles.slogan_3} >Same Installers, Same Panels. Better Prices</Text>
+            <View style={styles.btn_container}>
+              <Button
+                //onPress={this.buttonClickListener}
+                title="Get Free Proposal"
+                color="#FFFFFF"
+              />
+            </View>
           </View>
-        </View>
+          <Text style={styles.heading}>
+            Go solar in 3 easy steps 
+          </Text>
+        </ScrollView>
+        <View style={styles.separator}></View>
         <FlatList
           horizontal={ true } 
           data={ this.state.navibar_items }
@@ -60,10 +66,11 @@ export default class App extends Component<Props> {
                 style={styles.nav_btn_container}
                 onPress={() => this.handleNavBarTap(item)}
               >
-                <Text style={styles.nav_text}> {item.name} </Text>
+                <Text style={styles.nav_text}> {item.text} </Text>
               </TouchableOpacity>
           }
         />
+        
       </View>
     );
   }
@@ -76,6 +83,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  scrollable_container: {
+    width: '100%',
+    height: '75%',
+  },
   INSOLAR_logo: {
     alignItems: 'flex-start',
     width: '80%',
@@ -83,7 +94,7 @@ const styles = StyleSheet.create({
   },
   bg: {
     width: '100%',
-    height: 300,
+    height: 550,
   },
   bg_img: {
     width: '100%',
@@ -91,25 +102,27 @@ const styles = StyleSheet.create({
   },
   slogan_1: {
     position: 'absolute',
-    top: 50,
+    top: 70,
     left: 0,
     right: 0,
     fontSize: 40,
     textAlign: 'center',
     color: "#FFFFFF",
+    fontWeight: 'bold',
   },
   slogan_2: {
     position: 'absolute',
-    top: 100,
+    top: 130,
     left: 0,
     right: 0,
     fontSize: 40,
     textAlign: 'center',
     color: "#FFFFFF",
+    fontWeight: 'bold',
   },
   slogan_3: {
     position: 'absolute',
-    top: 175,
+    top: 350,
     left: 0,
     right: 0,
     fontSize: 18,
@@ -119,17 +132,30 @@ const styles = StyleSheet.create({
   btn_container: {
     width: "60%",
     position: 'absolute',
-    top: 220,
+    top: 420,
     left: 0,
     right: 0,
     marginLeft: 75,
     backgroundColor: "#54B948",
+    borderRadius: 5,
   },
   nav_btn_container: {
-    height: 70,
+    height: 30,
+    paddingTop: 3,
+    bottom: 0,
     flexDirection: 'row',
   },
   nav_text: {
     fontSize: 18,
+    textAlign: 'center',
+  },
+  heading: {
+    fontSize: 20,
+    margin: 10,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: 'lightgray',
   },
 });
