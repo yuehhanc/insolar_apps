13ca62ce8f02b1eea1b4ad427b6fb5a497781352
Yueh-Han <yuehhanc@andrew.cmu.edu>
2019-04-10 11:46:33 -0400

    About Us done

--- INSOLAR/AboutUsScreen.js
+++ INSOLAR/AboutUsScreen.js
@@ -7,45 +7,186 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Button, FlatList, TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Button, FlatList, TouchableOpacity, ScrollView} from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome5";
+import Icon2 from "react-native-vector-icons/FontAwesome";
+import Icon3 from "react-native-vector-icons/Ionicons";
+import Icon4 from "react-native-vector-icons/MaterialIcons";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+var styles = require('./style');
 
 type Props = {};
 export default class App extends Component<Props> {
   state = {
     navibar_items: [
-      {name: 'About Us'},
-      {name: 'Press'},
-      {name: 'Partners'},
-      {name: 'Resources'},
-      {name: 'Login'},
+      {name: 'AboutUs', text: 'About'},
+      {name: 'Press', text: 'Press'},
+      {name: 'Partners', text: 'Partners'},
+      {name: 'Resources', text: 'Resources'},
+      {name: 'Login', text: 'Login'},
+    ],
+    advantages: [
+      {text: 'Powered by AI'},
+      {text: '1-2-3 Sales Process'},
+      {text: 'Most Affordable Prices'},
+      {text: '100% Transparency'},
+    ],
+    why_us: [
+      {text: 'Save More'},
+      {text: 'Transparent Process'},
+      {text: 'Shortest Patback Period'},
+      {text: 'Simple Online Purchase'},
     ],
   }
 
   handleNavBarTap = item => {
     console.log(item.name);
+    this.props.navigation.navigate(item.name);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text> About Us </Text>
+        <ScrollView style={styles.scrollable_container}>
+          <View style={{width: '100%', height: 200, backgroundColor:'#00BFFF'}}>
+            <Text style={{alignSelf: 'center', fontSize: 40, color: 'white', fontWeight: 'bold', marginTop:75,}}> About Us </Text>
+          </View>
+          <View>
+            <Text style={styles.heading4}> Our Story </Text>
+            <View style={styles.separator3}></View>
+            <Text style={styles.content2}>
+              <Text style={{fontWeight: "bold"}}> INSOLAR </Text>
+               is a technology company that connects solar customers with the best solar panel installers at the lowest prices in the market."}
+            </Text>
+            <Text style={styles.content2}>
+              {"We’re a fast growing tech start-up of young scientists, programmers, and environmentalists who are passionate about the planet and eager to bring artificial intelligence methods to combat climate change."}
+            </Text>
+            <Text style={styles.content2}>
+              {"We’ve built the easiest and cheapest way to go solar and our platform has caught on. We started in Southern California in 2018 and have grown nationwide."}
+            </Text>
+          </View>
+
+
+          <View style={styles.bg_fourthblock} >
+            <View style={styles.bg_innerblock}>
+              <View style={styles.icon}>
+                <Icon2
+                  name="bullseye"
+                  color="#FF8C00"
+                  size={100}
+                />
+              </View>
+              <Text style={styles.heading5}>
+                Mission
+              </Text>
+              <Text style={styles.content2}>
+                {"Our mission is to make buying solar simple and efficient by connecting solar shoppers with the best panels, installers and lenders."}
+              </Text>
+              <View style={styles.icon}>
+                <Icon
+                  name="eye"
+                  color="#FF8C00"
+                  size={100}
+                />
+              </View>
+              <Text style={styles.heading5}>
+                Vision
+              </Text>
+              <Text style={styles.content2}>
+                {"We see INSOLAR as more than just the simplest, fastest, most affordable way to go solar. It’s a trans-formative solar adoption platform that has cut the cost of energy in half and is helping save the planets life."}
+              </Text>
+              <View style={styles.icon}>
+                <Icon
+                  name="sun"
+                  color="#FF8C00"
+                  size={100}
+                />
+              </View>
+              <Text style={styles.heading5}>
+                Technology
+              </Text>
+              <Text style={styles.content2}>
+                {"Artificial Intelligence lives at the core of INSOLAR’s existence. Our machine learning technology has eliminated 50% of the soft costs associated with purchasing solar, this is passed on to INSOLAR users."}
+              </Text>
+            </View>
+          </View>
+
+
+          <View style={styles.bg_fourthblock} >
+            <Text style={{color: '#FF69B4', fontSize: 12, alignSelf: 'center', marginTop: 30}}>
+              {"INSOLAR 1-2-3 PROCESS"}
+            </Text>
+            <Text style={styles.heading}>
+              We solve your problems
+            </Text>
+            <Text style={styles.content5}>
+              {"Save Time And Money. Access Hundreds Of Solar Installers And The Best Solar Financing Plans Available With One Smart Portal. You’ll get the best deal in the market"}
+            </Text>
+            <View style={styles.bg_innerblock}>
+              <View style={styles.icon}>
+                <Icon2
+                  name="flash"
+                  color="#FF69B4"
+                  size={100}
+                />
+              </View>
+              <Text style={styles.heading2}>
+                1
+              </Text>
+              <View style={styles.separator2}></View>
+              <Text style={styles.sm_heading}> FAST & EASY</Text>
+              <Text style={styles.content}>
+                {"Fill the form to receive your free customized solar proposal."}
+              </Text>
+              <Text style={styles.content}>
+                {"Our AI technology will design your solar energy system with the best solar panels at the cheapest price by the highest rated installers servicing your region."}
+              </Text>
+            </View>
+
+            <View style={styles.bg_innerblock}>
+              <View style={styles.icon}>
+                <Icon4
+                  name="computer"
+                  color="#FF69B4"
+                  size={100}
+                />
+              </View>
+              <Text style={styles.heading2}>
+                2
+              </Text>
+              <View style={styles.separator2}></View>
+              <Text style={styles.sm_heading}> GET THE BEST DEAL </Text>
+              <Text style={styles.content}>
+                {"Receive your proposal with all the details about cost, rebates, equipment, warranties, design, and installer."}
+              </Text>
+              <Text style={styles.content}>
+                {"You don’t need to move from your chair!"}
+              </Text>
+            </View>
+
+            <View style={styles.bg_innerblock}>
+              <View style={styles.icon}>
+                <Icon2
+                  name="dollar"
+                  color="#FF69B4"
+                  size={100}
+                />
+              </View>
+              <Text style={styles.heading2}>
+                3
+              </Text>
+              <View style={styles.separator2}></View>
+              <Text style={styles.sm_heading}> START SAVING </Text>
+              <Text style={styles.content}>
+                {"Electronically sign your paperwork and schedule your installation from your phone or email."}
+              </Text>
+              <Text style={styles.content}>
+                {"Track your usage and enjoy your savings."}
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
---
+++ INSOLAR/style.js
@@ -0,0 +1,199 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+} = React;
+
+module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  scrollable_container: {
+    width: '100%',
+    height: '75%',
+  },
+  INSOLAR_logo: {
+    alignItems: 'flex-start',
+    width: '80%',
+    height: 100,
+  },
+  bg: {
+    width: '100%',
+    height: 550,
+  },
+  bg_img: {
+    width: '100%',
+    height: '100%',
+  },
+  bg_thirdblock: {
+    backgroundColor: '#4682B4',
+    width: '100%',
+    padding: 15,
+  },
+  bg_innerblock: {
+    margin: 10,
+    backgroundColor: 'white',
+  },
+  bg_fourthblock: {
+    backgroundColor: '#DCDCDC',
+    width: '100%',
+    padding: 7,
+  },
+  article_block: {
+    margin: 10,
+    backgroundColor: '#F5FCFF',
+  },
+  slogan_1: {
+    position: 'absolute',
+    top: 70,
+    alignSelf: 'center',
+    fontSize: 40,
+    textAlign: 'center',
+    color: "#FFFFFF",
+    fontWeight: 'bold',
+  },
+  slogan_2: {
+    position: 'absolute',
+    top: 130,
+    alignSelf: 'center',
+    fontSize: 40,
+    textAlign: 'center',
+    color: "#FFFFFF",
+    fontWeight: 'bold',
+  },
+  slogan_3: {
+    position: 'absolute',
+    top: 350,
+    alignSelf: 'center',
+    fontSize: 18,
+    textAlign: 'center',
+    color: "#FFFFFF",
+  },
+  btn_container: {
+    width: "60%",
+    position: 'absolute',
+    top: 420,
+    alignSelf: 'center',
+    backgroundColor: "#54B948",
+    borderRadius: 5,
+  },
+  nav_btn_container: {
+    height: 30,
+    paddingTop: 3,
+    bottom: 0,
+    flexDirection: 'row',
+  },
+  nav_text: {
+    fontSize: 18,
+    color: 'gray',
+    textAlign: 'center',
+  },
+  sm_heading: {
+    fontSize: 14,
+    margin: 30,
+    textAlign: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 30,
+    textAlign: 'center',
+  },
+  heading2: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  heading3: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 30,
+    textAlign: 'center',
+    color: 'skyblue',
+  },
+  heading4: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 15,
+    textAlign: 'left',
+  },
+  heading5: {
+    color: 'gray',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: 'lightgray',
+  },
+  separator2: {
+    width: '10%',
+    height: 3,
+    backgroundColor: '#FF69B4',
+    alignSelf: 'center',
+  },
+  separator3: {
+    width: '90%',
+    height: 3,
+    backgroundColor: '#FF69B4',
+    alignSelf: 'center',
+  },
+  icon: {
+    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  content: {
+    fontSize:16,
+    color: 'gray',
+    marginBottom: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    textAlign: 'center',
+  },
+  content2: {
+    fontSize: 16,
+    textAlign: 'left',
+    margin: 18,
+  },
+  content3: {
+    fontSize:16,
+    color: 'gray',
+    marginBottom: 30,
+    marginLeft: 10,
+    marginTop: 20,
+    marginRight: 10,
+    textAlign: 'center',
+  },
+  content4: {
+    fontSize:16,
+    color: 'gray',
+    marginBottom: 30,
+    marginLeft: 10,
+    marginTop: 5,
+    marginRight: 10,
+    textAlign: 'center',
+  },
+  content5: {
+    fontSize:16,
+    color: 'gray',
+    marginBottom: 30,
+    marginLeft: 10,
+    marginTop: 20,
+    marginRight: 10,
+    textAlign: 'left',
+  },
+  
+});
